e313f1c62dc39c143eaae8b8ecc1a6a8ae0ecf7c
bblackshear <bblackshear@nextgxdx.com>
2011-12-11 12:36:44 -0600

    Added ability to specifiy file extensions

--- SublimeOnSaveBuild.py
+++ SublimeOnSaveBuild.py
@@ -2,4 +2,6 @@ import sublime, sublime_plugin
 
 class SublimeOnSaveBuild( sublime_plugin.EventListener ):
     def on_post_save( self, view ):
-        view.window().run_command( "build" )
+    	ext = view.file_name().split('.')[len(view.file_name().split('.'))-1]
+    	if ext in "styl coffee less".split(' '):
+        	view.window().run_command( "build" )
